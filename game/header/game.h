1529894100c9ccf89ab1883986529a301baff505
Leo040998 <52255401+Leo040998@users.noreply.github.com>
2019-06-27 17:25:18 -0300

    Add files via upload

---
+++ game/header/game.h
@@ -0,0 +1,170 @@
+/* UNIVERSIDADE FEDERAL DE SANTA CATARINA
+ * Marielly Silva Alberti,   17150212.
+ * Leonardo Nunes Rodrigues, 17250488.
+ * Projeto: Adventurer's Quest.
+ */
+
+#if !defined GAME_H
+#define GAME_H
+
+#include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+    void audio(int);
+    
+    void print_menu(int);
+    
+    void ler_pag();
+    
+    void print_batalha1A(int, int, int);
+    
+    void print_batalha1B(int, int, int);
+    
+    void print_batalha2A(int, int, int);
+    
+    void print_batalha2B(int, int, int);
+    
+    void print_batalha3A(int, int, int);
+    
+    void print_batalha3B(int, int, int);
+    
+    void print_batalha4A(int, int, int);
+    
+    void print_batalha4B(int, int, int);
+    
+    void print_batalha4C(int, int, int);
+    
+    void Pg_001();
+    
+    void Pg_002();
+    
+    void Pg_003();
+    
+    void Pg_004();
+    
+    void Pg_005();
+    
+    void Pg_006();
+    
+    void Pg_007();
+    
+    void Pg_008();
+    
+    void Pg_009();
+    
+    void Pg_010();
+    
+    void Pg_011();
+    
+    void Pg_012();
+    
+    void Pg_013();
+    
+    void Pg_014();
+    
+    void Pg_015();
+    
+    void Pg_016();
+    
+    void Pg_017();
+    
+    void Pg_018();
+    
+    void Pg_019();
+    
+    void Pg_020();
+    
+    void Pg_021();
+    
+    void Pg_022();
+    
+    void Pg_023();
+    
+    void Pg_024();
+    
+    void Pg_025();
+    
+    void Pg_026();
+    
+    void Pg_027();
+    
+    void Pg_028();
+    
+    void Pg_029();
+    
+    void Pg_030();
+    
+    void Pg_031();
+    
+    void Pg_032();
+    
+    void Pg_033();
+    
+    void Pg_034();
+    
+    void Pg_035();
+    
+    void Pg_036();
+    
+    void Pg_037();
+    
+    void Pg_038();
+    
+    void Pg_039();
+    
+    void Pg_040();
+    
+    void Pg_041();
+    
+    void Pg_042();
+    
+    void Pg_043();
+    
+    void Pg_044();
+    
+    void Pg_045();
+        
+    void Pg_046();
+    
+    void Pg_047();
+    
+    void Pg_048();
+    
+    void Pg_049();
+    
+    void Pg_050();
+    
+    void Pg_051();
+        
+    void Pg_052();
+    
+    void Pg_053();
+    
+    void Pg_054();
+    
+    void Pg_055();
+    
+    void Pg_056();
+    
+    void Pg_057();
+    
+    void Pg_058();
+    
+    void Pg_059();
+    
+    void Pg_060();
+    
+    void Pg_061();
+    
+    void Pg_062();
+    
+    void Pg_063();
+    
+    void Pg_064();
+    
+    void Pg_Fim_de_Jogo();
+    
+#endif
